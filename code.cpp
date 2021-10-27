d6a051030b6051a5ea8c5abf3ad7f422b47e02c4
satyapal1234 <41913624+satyapal1234@users.noreply.github.com>
2021-10-27 06:37:57 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+/*
+commenting for better rich
+ Follow this .
+1. variable name should be sync with the actual definition
+2. indentation is necessary for code to look healthy
+3. avoid multiple cout and cin*/
 int main()
 {
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
